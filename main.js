059a887f7a4bd5eff747e6e8bf0696a03dd2053a
Leowell Bacudio <46722450+leowellb@users.noreply.github.com>
2020-04-27 03:27:07 -0400

    minor changes

--- main.js
+++ main.js
@@ -2,15 +2,33 @@
 let mainNav=document.getElementById('main-nav');
 let navbarToggle=document.getElementById('navbar-toggle');
 
+navbarToggle.addEventListener("mouseover", function(){
+
+    if(!this.classList.contains('active')){
+        this.style.color="#F4880A";
+    }
+});
+
+navbarToggle.addEventListener("mouseout", function(){
+
+    if(this.classList.contains('active')){
+        this.style.color="#F4880A";
+    }
+    else{
+        this.style.color="black";
+    }
+});
+
 navbarToggle.addEventListener('click',function(){
 
     if(this.classList.contains('active')){
         mainNav.style.display="none";
         this.classList.remove('active');
+        this.style.color="black";
     }
     else{
         mainNav.style.display="flex";
         this.classList.add('active');
-
+        this.style.color="#F4880A";
     }
 });
